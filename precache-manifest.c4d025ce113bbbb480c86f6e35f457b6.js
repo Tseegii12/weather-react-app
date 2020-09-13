838d4f634a796e22c42451aa2fd16d9dda6d4722
Tseegii12 <wahrom.nabiev@gmail.com>
2020-09-14 00:18:36 +0800

    Updates

---
+++ precache-manifest.c4d025ce113bbbb480c86f6e35f457b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d8d5d36c81a8acdb35ea35b938689f",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "1046e52060a9f63ee380",
+    "url": "/weather-react-app/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "ed90fd4a9f4660699627",
+    "url": "/weather-react-app/static/css/main.a5d9cb33.chunk.css"
+  },
+  {
+    "revision": "1046e52060a9f63ee380",
+    "url": "/weather-react-app/static/js/2.44466954.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather-react-app/static/js/2.44466954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed90fd4a9f4660699627",
+    "url": "/weather-react-app/static/js/main.2736e30c.chunk.js"
+  },
+  {
+    "revision": "a3056d7f65192abfd053",
+    "url": "/weather-react-app/static/js/runtime-main.9b34687e.js"
+  }
+]);
